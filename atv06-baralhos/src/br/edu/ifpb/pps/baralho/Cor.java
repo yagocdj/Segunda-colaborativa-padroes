03da178f7a91ebf6ff664ef81d15e436fe519da4
Yago César <yagocdj@gmail.com>
2024-09-11 22:16:29 -0300

    feat: progredindo na criação do diagrama (implementando o padrão builder)

--- atv06-baralhos/src/br/edu/ifpb/pps/baralho/Cor.java
+++ atv06-baralhos/src/br/edu/ifpb/pps/baralho/Cor.java
@@ -4,7 +4,9 @@ package br.edu.ifpb.pps.baralho;
 public enum Cor {
 	//
 	VERMELHO("\u001B[31m","vermelho"),
-	PRETO("\u001B[30m", "preto"), 
+	PRETO("\u001B[30m", "preto"),
+	AMARELO("\u001B[33m", "amarelo"),
+	VERDE("\u001B[32m", "verde"),
 	RESET("\u001B[0m", "reset");
 	
 	private final String ansiCode;
