5c9eef3787b261a460ebeb8667a3d7cf6d358d93
Renato Melo <renato.bmelo@outlook.com>
2024-09-15 02:16:19 -0300

    feat: mudanca carta

--- src/carta/Carta.java
+++ src/carta/Carta.java
@@ -1,17 +1,26 @@
 package carta;
 
 import enums.Cor;
+import java.util.List;
 
 public abstract class Carta {
     private String face;
-    private Cor cor;
+    private List<Cor> cores; // Múltiplas cores
     private int valor;
 
-    public Carta(){}
+    public Carta() {}
 
+    // Construtor para múltiplas cores
+    public Carta(String face, List<Cor> cores, int valor) {
+        this.face = face;
+        this.cores = cores;
+        this.valor = valor;
+    }
+
+    // Construtor para uma única cor (compatibilidade com subclasses que usam uma cor)
     public Carta(String face, Cor cor, int valor) {
         this.face = face;
-        this.cor = cor;
+        this.cores = List.of(cor); // Armazenar como lista com um único elemento
         this.valor = valor;
     }
 
@@ -19,14 +28,19 @@ public abstract class Carta {
         return face;
     }
 
-    public Cor getCor() {
-        return cor;
+    public List<Cor> getCores() {
+        return cores;
     }
 
     public int getValor() {
         return valor;
     }
 
+    // Método para obter a primeira cor (caso seja uma lista com uma única cor)
+    public Cor getCor() {
+        return cores.get(0);
+    }
+
     @Override
-    public abstract  String toString();
+    public abstract String toString();
 }
--- src/carta/CartaUno.java
+++ src/carta/CartaUno.java
@@ -1,16 +1,38 @@
 package carta;
 
+import java.util.List;
+
 import enums.Cor;
 
 public class CartaUno extends Carta {
+
+    private List<Cor> cores;
+    
     public CartaUno() { }
 
     public CartaUno(String face, int valor, Cor cor) {
         super(face, cor, valor);
+        this.cores = List.of(cor);
+    }
+
+    public CartaUno(String face, int valor, List<Cor> cores) {
+        super(face, cores, valor);
+    }
+
+    public List<Cor> getCores() {
+        return cores;
     }
 
     @Override
     public String toString() {
-        return super.getFace() + " de cor " + super.getCor();
+        if (cores.size() == 1) {
+            return String.format("%6s", super.getFace()) + " de cor " + cores.get(0).ansiCode() + cores.get(0) + Cor.RESET.ansiCode() + ", valor " + super.getValor();
+        } else {
+            StringBuilder coresString = new StringBuilder();
+            for (Cor cor : cores) {
+                coresString.append(cor.ansiCode()).append(cor).append(Cor.RESET.ansiCode()).append(" ");
+            }
+            return String.format("%6s", super.getFace()) + " com cores " + coresString.toString() + ", valor " + super.getValor();
+        }
     }
 }
